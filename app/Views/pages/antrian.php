b67f8610044cea67fbe2c21ce4f31157a7f321f9
Komarudin <101499377+komarcrew@users.noreply.github.com>
2022-07-10 10:45:21 +0700

    Add files via upload

---
+++ app/Views/pages/antrian.php
@@ -0,0 +1,52 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<link rel="stylesheet" type="text/css" href="style.css">
+<div class="section">
+    <div class="container">
+        <iframe width="700" height="350px"
+src="http://2.bp.blogspot.com/_yEEVc8Ixmow/SnZ9N-B-hrI/AAAAAAAAABg/VcC7SnHO4kA/w1200-h630-p-k-no-nu/roko.png">
+</iframe>
+        <aside id="sidebar">
+
+    <div class="widget-box">
+        <center>
+            <h5 class="title"></h5>
+            <h1>
+                1
+            </h1>
+            
+        <button class="btn btn-warning">Loket 1</button>
+        </center>
+       
+
+            </div>
+
+    <div class="widget-box">
+        <center>
+            <h5 class="title"></h5>
+            <h1>
+                A02
+            </h1>
+            
+        <button class="btn btn-warning">Loket 2</button>
+        </center>
+        <div class="widget-box">
+        <center>
+            <h5 class="title"></h5>
+            <h1>
+                A03
+            </h1>
+            
+        <button class="btn btn-warning">Loket 3</button>
+        </center>
+    </div>
+</aside>
+</div>
+</div>
+
+        </div>
+    </div>
+    <br>
+    <br>
+    <?= $this->endSection(); ?>
